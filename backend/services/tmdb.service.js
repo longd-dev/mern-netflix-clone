c8c7fe118cae1b3428b2e91e3bbf64184384fcd4
longd-dev <longduong325@gmail.com>
2025-05-26 15:01:26 -0700

    initial commit

---
+++ backend/services/tmdb.service.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { ENV_VARS } from "../config/envVars.js";
+
+const url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+
+export const fetchFromTMDB = async (url) => {
+  const options = {
+    headers: {
+      accept: "application/json",
+      Authorization: "Bearer " + ENV_VARS.TMDB_API_KEY,
+    },
+  };
+
+  const response = await axios.get(url, options);
+
+  if (response.status !== 200) {
+    throw new Error("Failed to fetch data from TMDB" + response.statusText);
+  }
+
+  return response.data;
+};
